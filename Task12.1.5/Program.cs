e8b249378ec32b35c4b38e357acd5bd9a28de132
Bochanikita <bochanikita@yandex.ru>
2022-12-03 15:38:35 +0300

    Add project files.

---
+++ Task12.1.5/Program.cs
@@ -0,0 +1,25 @@
+﻿string name;
+string login;
+string premium;
+bool prem;
+
+User newUser = new User();
+
+Console.WriteLine("Enter your name:");
+newUser.Name = Console.ReadLine();
+Console.WriteLine("Enter your login:");
+newUser.Login = Console.ReadLine();
+Console.WriteLine("Do you have a premium? Y(yes) or N(not)");
+premium = Console.ReadLine();
+
+Console.WriteLine($"Hello {newUser.Name}!");
+
+if (premium == "Y")
+    newUser.IsPremium = true;
+else if (premium == "N")
+{
+    newUser.IsPremium = false;
+    prem = newUser.IsPremium;
+}
+else
+    Console.WriteLine("Enter Y or N, please");
---
+++ Task12.1.5/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class User
+{
+    private string login;
+    private string name;
+    private bool premium;
+
+    public User()
+    {
+    }
+
+    public string Login { get { return login; } set { login = value; } }
+    public string Name { get { return name; } set { name = value; } }
+    public bool IsPremium
+    {
+        get
+        {
+            if(premium == true)
+                return true;
+            else
+            {
+                ShowAds();
+                return false;
+            }
+        }
+        set { premium = value; }
+    }
+
+    void ShowAds()
+    {
+        Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        // Остановка на 1 с
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+        // Остановка на 2 с
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+        // Остановка на 3 с
+        Thread.Sleep(3000);
+    }
+}
